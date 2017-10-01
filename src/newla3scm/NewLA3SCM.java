fe564d2cc9634b00a92841c64c893ab5d41110d3
fenixobia <g00064030@aus.edu>
2017-10-01 21:43:31 +0400

    Initial Commit

---
+++ src/newla3scm/NewLA3SCM.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newla3scm;
+
+/**
+ *
+ * @author Khalid Khan
+ */
+public class NewLA3SCM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
