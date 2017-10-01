c5a5f58f7b3e185eee516833770df26def8085b8
nr901 <nberry903@gmail.com>
2017-10-01 23:15:19 +0400

    Feature B added

--- src/newla3scm/NewLA3SCM.java
+++ src/newla3scm/NewLA3SCM.java
@@ -9,14 +9,18 @@ package newla3scm;
  *
  * @author Khalid Khan
  */
+class addFeaturB{
+public addFeaturB(){};
+public void adFeatureB(){System.out.println("Add Feature B");};
+}
 public class NewLA3SCM {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        addFeaturB afb=new addFeaturB();
+        afb.adFeatureB();
     }
-    
 }
